73d946a797b623b315a7522a13a09d4def5dc35f
govind6498 <govindsingh6498@gmail.com>
2021-09-13 01:32:28 +0530

    create:find out the Quetient and remainder.

---
+++ src/com/bridgelabz/basiccoreprograms/ComputeQuotientAndRemainder.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class ComputeQuotientAndRemainder {
+	public static void main(String[] args) {
+		int divident=37,divisor=5,qutient,remainder;
+		qutient=divident/divisor;
+		remainder=divident%divisor;
+		System.out.println("Divident:"+divident);
+		System.out.println("Divisor:"+divisor);
+		System.out.println("Quotient:"+qutient);
+		System.out.println("Remainder:"+remainder);
+		
+		
+	}
+}
