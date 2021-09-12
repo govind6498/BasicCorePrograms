0039320ec9b1998e80c65e498569fcbf5485d7c8
govind6498 <govindsingh6498@gmail.com>
2021-09-13 01:35:36 +0530

    find out number is even or Odd

---
+++ src/com/bridgelabz/basiccoreprograms/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		int Number=201;
+		if(Number%2==0) {
+			System.out.println(Number +" is Even");
+			System.exit(0);
+		}
+		System.out.println(Number+" is Odd");
+	}
+}
